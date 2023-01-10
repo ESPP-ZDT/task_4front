bbf7cb7ea43f6f71ad0a35884c0edcd35a886a0f
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-10 22:47:45 +0100

    changed api url for deploy

--- src/Variables.js
+++ src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-    API_URL:"https://task-back.onrender.com",
+    API_URL:"https://railway.app/project/a3fb3e13-7268-4510-ae73-97a678907826",
     PHOTO_URL:"http://localhost:49146/Photos/"
 }
