5c38983dc8b3620284407e986c5a269570685d59
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-10 23:09:42 +0100

    changed api url for deploy

--- src/Variables.js
+++ src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-    API_URL:"https://railway.app/project/a3fb3e13-7268-4510-ae73-97a678907826",
+    API_URL:"task4back-production.up.railway.app",
     PHOTO_URL:"http://localhost:49146/Photos/"
 }
