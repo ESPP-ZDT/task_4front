eb76730362784b271b1d241f9c31b784906ebb9e
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-11 16:47:06 +0100

    updated variables

--- src/Variables.js
+++ src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-    API_URL:"http://task4back-production-0f00.up.railway.app/",
+    API_URL:"http://task4back-production-0f00.up.railway.app/api/",
     PHOTO_URL:"http://localhost:49146/Photos/"
 }
