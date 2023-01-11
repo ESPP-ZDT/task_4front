6c3a2e488e9d8fdb0b883aed82a716780e560709
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-11 15:03:28 +0100

    changed the url again

--- src/Variables.js
+++ src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-    API_URL:"task4back-production.up.railway.app/",
+    API_URL:"https://task4back-production.up.railway.app/",
     PHOTO_URL:"http://localhost:49146/Photos/"
 }
